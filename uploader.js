68b77c2f9ebb30d3247f10a7766955c7ef4a368a
Nitish <badboynitish755@gmail.com>
2026-03-01 00:38:16 +0530

    final working scheduler

--- uploader.js
+++ uploader.js
@@ -1,4 +1,4 @@
-async function uploadVideo(filePath, type) { console.log(Uploading ${type} video: ${filePath});
-// Yaha pe tum YouTube API code daaloge // Abhi ke liye simulation
-console.log(${type} Video Uploaded Successfully 🚀); }
+async function uploadVideo(filePath, type) { console.log("Uploading " + type + " video: " + filePath);
+// Yaha pe YouTube API code aayega future me // Abhi ke liye simulation
+console.log(type + " Video Uploaded Successfully"); }
 module.exports = { uploadVideo };
